fa12103c818a8f4e198fb9d2ed2c128c0c732183
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2025-04-01 12:25:05 +0200

    Folder structure with gentric classes but no implementation

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf63a9b93f916caf0c6ee063415b067bc325d40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a794885d8e9a91aed2bfa402be6d7fb99b409a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Infrastructure/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Data;
+
+namespace Infrastructure.Interfaces
+{
+
+    public interface IGenericRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+
+    }
+
+}
